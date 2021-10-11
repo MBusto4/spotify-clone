3480f2c0cbd72ddf2fccd5f6e5c9dd654c1aa965
MBusto <mikebusto4@gmail.com>
2021-10-11 16:27:16 -0400

    feat:able to user the user's info after log in

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Login from './Login';
 import React, { useEffect, useState } from "react"
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+//creating and instance of spotify in our app
+const spotify = new SpotifyWebApi()
 
 function App() {
   //creating some state
-
   const [token, setToken] = useState(null)
-
-
-
 
   //run code based on a given condition
   useEffect(() => {
@@ -20,6 +20,11 @@ function App() {
     const _token = hash.access_token
     if (_token) {
       setToken(_token)
+      //giving the token to the spotify api
+      spotify.setAccessToken(_token)
+      spotify.getMe().then(user => {
+        console.log("USER---->", user)
+      })
     }
     console.log("THE TOKEN IS----> ", token)
   }, [])
