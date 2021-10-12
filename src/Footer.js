e74e9b7bbb2d392a6752ada66c635984cdea2d21
MBusto <mikebusto4@gmail.com>
2021-10-12 15:25:11 -0400

    styling the play button with react-icons

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Footer.css'
+import { BsPlayCircle } from "react-icons/bs";
+import { BsFillSkipBackwardFill } from "react-icons/bs";
+import { BsFillSkipForwardFill } from "react-icons/bs";
+import { BsShuffle } from "react-icons/bs";
+import { BsArrowRepeat } from "react-icons/bs";
 
 function Footer() {
     return (
         <div className='footer'>
-            <h1>Hello from footer</h1>
+            <div className="footer__left">
+                <p>Album & Song Details</p>
+            </div>
+
+            <div className="footer__center">
+                <BsShuffle className='footer__green' />
+                <BsFillSkipBackwardFill className='footer__icon' />
+                <BsPlayCircle size={50} className='footer__icon footer__play' />
+                <BsFillSkipForwardFill className='footer__icon' />
+                <BsArrowRepeat className='footer__green' />
+            </div>
+
+            <div className="footer_right">
+                <p>Volume Controls</p>
+            </div>
         </div>
     )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
     isPlaying: false,
     item: null,
     //remove after developing allows us to stay logged in
-    // token: 'BQCbb0ZJf5Ho3PRcB0BDWGzk5N4l0XyGYdVbzdAvHw4uCnSqCDm3DBikP2qCeBLkhd2_BGdwChrFnehGdpOdH5dz4YGlTamZVKlGvaRzP8wfe0RDndnmXYxO3qHv7-wrw-E1SopVzG03-6rrrqq0jJfi7-sHEl-P-fyhKU2e2CakB3pY1L72'
+    token: 'BQCbb0ZJf5Ho3PRcB0BDWGzk5N4l0XyGYdVbzdAvHw4uCnSqCDm3DBikP2qCeBLkhd2_BGdwChrFnehGdpOdH5dz4YGlTamZVKlGvaRzP8wfe0RDndnmXYxO3qHv7-wrw-E1SopVzG03-6rrrqq0jJfi7-sHEl-P-fyhKU2e2CakB3pY1L72'
 }
 
 const reducer = (state, action) => {
